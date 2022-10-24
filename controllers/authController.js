50326fe93ddf443cd99c29fad696525da966decc
ravzkie12 <ortiz.raven@dnsc.edu.ph>
2022-10-24 16:49:22 +0800

    cookie duration adjust

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,7 +33,7 @@ const handleErrors = (err) => {
   }
   return errors;
 }
-const maxAge = 3 * 24 * 60 * 60;
+const maxAge = 1 * 24 * 60 * 60;
 const createToken = (id) => {
   return jwt.sign({ id }, secret, {
     expiresIn: maxAge
